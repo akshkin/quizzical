f32ccc0a3c2b5b311aa84c724bc19a53aa31efdb
akshkin <107682289+akshkin@users.noreply.github.com>
2022-09-18 11:58:54 +0200

    changed text on start page

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -4,7 +4,7 @@ export default function StartPage(props){
           <main>
             <div className="start-page">
                 <h1 className="main-title">Quizzical</h1>
-                <p className="main-subtitle">Some description if needed</p>
+                <p className="main-subtitle">Time to test you general knowledge!</p>
                 <button className="btn" onClick={props.play}>Start quiz</button> 
             </div>
         </main>
